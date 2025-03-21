d9094297f02924b192d2403bb015e357a2460013
hairethan12 <153117354+hairethan12@users.noreply.github.com>
2025-03-21 14:41:08 -0600

    Removed .vs folder

---
+++ backend/Mission_11Hair.API/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission_11Hair.API.Data;
+
+namespace Mission_11Hair.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+        public BookController(BookDbContext temp)
+        {
+            _bookContext = temp;
+        }
+
+        [HttpGet("AllBooks")]
+        public IActionResult GetBooks(int pageSize = 10, int pageNum = 1)
+        {
+
+            var something = _bookContext.Books
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalNumBooks = _bookContext.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                totalNumBooks = totalNumBooks
+            };
+            return Ok(someObject);
+        }
+    }
+}
---
+++ backend/Mission_11Hair.API/Data/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission_11Hair.API.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
